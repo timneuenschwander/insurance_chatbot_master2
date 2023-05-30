dbb3ae67b3b80872bf10f7cfcf3c85f874b161e3
timneuenschwander <63901857+timneuenschwander@users.noreply.github.com>
2023-05-30 11:24:02 +0200

    yeee

--- ChatbotFinal/chatbot.py
+++ ChatbotFinal/chatbot.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import webbrowser
 
 # Set up OpenAI API credentials
-openai.api_key = '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'
+openai.api_key = '...'
 
 # Read the system message from a file
 with open('TrainDataSympany.txt', 'r') as f:
